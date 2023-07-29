1896fe22179fcb1714a664a0d08bbb85b1fca2b0
YasharPa <yasharpashaiy1@gmail.com>
2023-07-29 12:33:34 +0300

    Implemetnig Skeleton component .

--- src/components/GagListItem.js
+++ src/components/GagListItem.js
@@ -1,11 +1,13 @@
 import { GoTrash, GoPencil } from "react-icons/go";
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import GagItemEdit from "./GagItemEdit";
 import GagsContext from "../context/gags";
+import Skeleton from "./Skeleton";
 
 function GagListItem({ gag }) {
   const { deleteGagById } = useContext(GagsContext);
   const [showEdit, setShowEdit] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   const handleDeleteGag = () => {
     deleteGagById(gag.id);
@@ -19,6 +21,9 @@ function GagListItem({ gag }) {
     setShowEdit(false);
   };
 
+  useEffect(() => {
+    setTimeout(() => setIsLoading(false), 2000);
+  }, [gag]);
   let contant = (
     <>
       <div>
@@ -27,25 +32,33 @@ function GagListItem({ gag }) {
       <img alt="Gags" src={gag.url} className="gag-image" />
     </>
   );
+
   if (showEdit) {
     contant = <GagItemEdit onSubmit={handleSubmit} gag={gag} />;
   }
 
   return (
     <div className="gag-item">
-      <GoTrash
-        className="trashcan-icon"
-        onClick={() => {
-          handleDeleteGag(gag.id);
-        }}
-      />
-      <GoPencil
-        className="pencil-icon"
-        onClick={() => {
-          handleEditGag();
-        }}
-      />
-      <div className="gag-contant">{contant}</div>
+      <label>
+        <GoTrash
+          className="trashcan-icon"
+          onClick={() => {
+            handleDeleteGag(gag.id);
+          }}
+        />
+        <GoPencil
+          className="pencil-icon"
+          onClick={() => {
+            handleEditGag();
+          }}
+        />
+      </label>
+
+      {isLoading ? (
+        <Skeleton times={3} className="active-skeleton" />
+      ) : (
+        <div className="gag-contant">{contant}</div>
+      )}
     </div>
   );
 }
--- src/components/Skeleton.js
+++ src/components/Skeleton.js
@@ -2,14 +2,14 @@ import classNames from "classnames";
 
 function Skeleton({ times, className }) {
   const outerClassName = classNames("outer-skeleton", className);
-  const innerClassName = classNames("inner-skeleton-shimmer");
+  const innerClassName = classNames("inner-skeleton");
 
   const boxes = Array(times)
     .fill(0)
     .map((_, index) => {
       return (
         <div key={index} className={outerClassName}>
-          <div className={innerClassName}></div>
+          <div className={innerClassName} />
         </div>
       );
     });

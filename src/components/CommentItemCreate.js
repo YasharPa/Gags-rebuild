aa9c969911a32bda51ded044dd9ffba9b2537ed3
YasharPa <yasharpashaiy1@gmail.com>
2023-08-08 00:18:27 +0300

    Comment input focus Added.

--- src/components/CommentItemCreate.js
+++ src/components/CommentItemCreate.js
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import GagsContext from "../context/gags";
 import Button from "./Button";
 
@@ -6,16 +6,24 @@ function CommentItemCreate({ gag }) {
   const { createComment } = useContext(GagsContext);
   const [comment, setComment] = useState("");
   const [showInput, setShowInput] = useState(true);
+  const inputRef = useRef();
 
   const handleCommentChange = (event) => {
     setComment(event.target.value);
   };
 
+  useEffect(() => {
+    if (showInput) {
+      inputRef.current.focus();
+    }
+  }, [showInput]);
   const handleCreate = () => {
     createComment(gag, comment);
     setComment("");
   };
-  let contant = <></>;
+
+  let contant;
+
   if (showInput) {
     contant = (
       <form
@@ -31,6 +39,7 @@ function CommentItemCreate({ gag }) {
           onChange={handleCommentChange}
           placeholder="Write a Comment"
           value={comment}
+          ref={inputRef}
         ></input>
         <Button secondary rounded>
           Post
